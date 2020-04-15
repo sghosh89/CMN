504176bc8fead1322d1015893ea756a16d21d072
sghosh89 <ghoshshyamolina89@gmail.com>
2020-04-15 02:46:39 -0500

    edits

--- ARMN_plotter.R
+++ ARMN_plotter.R
@@ -29,49 +29,52 @@ Plotter_AR<-function(f,KM,KN,Meq,Neq,eM=0.5,eN=0.5,aM=0.1,aN=0.2,bmax=0.8,d=0.5,
   sN<- sN0*alpha #slope for non-mutualist when plot A vs. R
   
   #------------------------- A vs. R plot -------------------------------------------------
-  pdf(paste(resloc,nametag,"f_",f,"_KM_",KM,"_KN_",KN,"_A_vs_R.pdf",sep=""),width=8,height=8)
+  #pdf(paste(resloc,nametag,"f_",f,"_KM_",KM,"_KN_",KN,"_A_vs_R.pdf",sep=""),width=8,height=8)
   
-  op<-par(mar=c(6,6,2,2),pty="s")
+  #op<-par(mar=c(6,6,2,2),pty="s")
   
-  plot(NA,xlim=xlm,ylim=ylm,xlab="R",ylab="A",cex.lab=2.5,cex.axis=2)
+  #plot(NA,xlim=xlm,ylim=ylm,
+  #     xlab="x",ylab="y",
+       #xlab="R",ylab="A",
+  #     cex.lab=2.5,cex.axis=2)
   
-  if(plot_MZNGI==T & plot_NZNGI==T){
-    abline(a=iM, b=sM,col="red",lwd=2)
-    abline(a =iN, b=sN,col="blue",lwd=2)
-    legend("topright", c("Mutualist ZNGI","Non-mutualist ZNGI"), col = c("red", "blue"),
-           cex = 2.5, lty = c(1, 1), lwd=c(2,2), xpd = TRUE, horiz = F, inset = c(0,0),y.intersp = 0.8,x.intersp = 0.1,
-           bty = "n") 
-  }
+#  if(plot_MZNGI==T & plot_NZNGI==T){
+#    abline(a=iM, b=sM,col="red",lwd=2)
+#    abline(a =iN, b=sN,col="blue",lwd=2)
+#    legend("topright", c("Mutualist ZNGI (Eqn. 8)","Non-mutualist ZNGI (Eqn. 9)"), col = c("red", "blue"),
+#           cex = 2.5, lty = c(1, 1), lwd=c(2,2), xpd = TRUE, horiz = F, inset = c(0,0),y.intersp = 0.8,x.intersp = 0.1,
+#           bty = "n") 
+#  }
 
-  if(plot_MZNGI==T){
-    abline(a=iM, b=sM,col="red",lwd=2)
-    legend("topright", c("Mutualist ZNGI"), col = c("red"),
-           cex = 2.5, lty = c(1), lwd=c(2), xpd = TRUE, horiz = F, inset = c(0,0),y.intersp = 0.8,x.intersp = 0.1,
-           bty = "n") 
-  }
+ # if(plot_MZNGI==T){
+#    abline(a=iM, b=sM,col="red",lwd=2)
+#    legend("topright", c("Mutualist ZNGI (Eqn. 8)"), col = c("red"),
+#           cex = 2.5, lty = c(1), lwd=c(2), xpd = TRUE, horiz = F, inset = c(0,0),y.intersp = 0.8,x.intersp = 0.1,
+#           bty = "n") 
+#  }
   
-  if(plot_NZNGI==T){
-    abline(a =iN, b=sN,col="blue",lwd=2)
-    legend("topright", c("Non-mutualist ZNGI"), col = c("blue"),
-           cex = 2.5, lty = c(1), lwd=c(2), xpd = TRUE, horiz = F, inset = c(0,0),y.intersp = 0.8,x.intersp = 0.1,
-           bty = "n") 
-  }
+ #if(plot_NZNGI==T){
+#    abline(a =iN, b=sN,col="blue",lwd=2)
+#    legend("topright", c("Non-mutualist ZNGI (Eqn. 9)"), col = c("blue"),
+#           cex = 2.5, lty = c(1), lwd=c(2), xpd = TRUE, horiz = F, inset = c(0,0),y.intersp = 0.8,x.intersp = 0.1,
+#           bty = "n") 
+#  }
   
   
-  abline(h=0,col="dimgrey",lty="dotted")
-  abline(v=0,col="dimgrey",lty="dotted")
+  #abline(h=0,col="dimgrey",lty="dotted")
+ # abline(v=0,col="dimgrey",lty="dotted")
   #grid()
   
-  colnames(x1)<-c("time","A","R")
-  lines(x1$R,x1$A,col="black",lty="dashed",lwd=2)
+#  colnames(x1)<-c("time","A","R")
+ # lines(x1$R,x1$A,col="black",lty="dashed",lwd=2)
  
   
   #arrows(x1$R[which(1:nrow(x1) %% n == 0)-0.1], x1$A[which(1:nrow(x1) %% n == 0)-0.5], 
   #       x1$R[1:nrow(x1) %% n == 0], x1$A[1:nrow(x1) %% n == 0] - 0.01, angle=40, 
   #       length=0.1, col="black",lwd=2)
 
-  par(op)
-  dev.off()
+  #par(op)
+  #dev.off()
   
   #------------------------------ A/alpha vs. R plot -----------------------------------------------
   
@@ -79,27 +82,30 @@ Plotter_AR<-function(f,KM,KN,Meq,Neq,eM=0.5,eN=0.5,aM=0.1,aN=0.2,bmax=0.8,d=0.5,
   
   op<-par(mar=c(6,7,2,2),pty="s",mgp=c(3.5,1,0))
   
-  plot(NA,xlim=xlm,ylim=ylm/alpha,xlab=expression(hat(R)),ylab=bquote(I == hat(A)/hat(alpha)),cex.lab=2.5,cex.axis=2)
-  #plot(NA,xlim=xlm,ylim=ylm/alpha,xlab="R",ylab=expression(hat(A)/hat(alpha)),cex.lab=2.5,cex.axis=2)
+  plot(NA,xlim=xlm,ylim=ylm/alpha,
+       xlab="x",ylab="y",
+       #xlab=expression(hat(R)),ylab=bquote(I == hat(A)/hat(alpha)),
+       cex.lab=2.5,cex.axis=2)
+ 
   
   if(plot_MZNGI==T & plot_NZNGI==T){
     abline(a=iM0, b=sM0,col="red",lwd=2)
     abline(a =iN0, b=sN0,col="blue",lwd=2)
-    legend("topright", c("Mutualist","Non-mutualist"), col = c("red", "blue"),
+    legend("topright", c("Mutualist ZNGI (Eqn. 8)","Non-mutualist ZNGI (Eqn. 9)"), col = c("red", "blue"),
            cex = 2.5, lty = c(1, 1), lwd=c(2,2), xpd = TRUE, horiz = F, inset = c(0,0),y.intersp = 0.8,x.intersp = 0.1,
            bty = "n") 
   }
   
   if(plot_MZNGI==T){
     abline(a=iM0, b=sM0,col="red",lwd=2)
-    legend("topright", c("Mutualist ZNGI"), col = c("red"),
+    legend("topright", c("Mutualist ZNGI (Eqn. 8)"), col = c("red"),
            cex = 2.5, lty = c(1), lwd=c(2), xpd = TRUE, horiz = F, inset = c(0,0),y.intersp = 0.8,x.intersp = 0.1,
            bty = "n") 
   }
   
   if(plot_NZNGI==T){
     abline(a =iN0, b=sN0,col="blue",lwd=2)
-    legend("topright", c("Non-mutualist ZNGI"), col = c("blue"),
+    legend("topright", c("Non-mutualist ZNGI (Eqn. 9)"), col = c("blue"),
            cex = 2.5, lty = c(1), lwd=c(2), xpd = TRUE, horiz = F, inset = c(0,0),y.intersp = 0.8,x.intersp = 0.1,
            bty = "n") 
   }
@@ -170,18 +176,18 @@ Plotter_AR(f=f,KM=10,KN=10,Meq,Neq,eM=0.5,eN=0.5,aM=0.1,aN=0.2,bmax=0.8,d=0.5,s=
 # ------------------ call the function for --------------- KM is not equal to KN ---------------
 
 # ----------------- when fmin < f < fmax ----------
-f<-0.6
-xlm<-c(0,450)
-ylm<-c(0,50)
-x1<-read.delim("./ARMN_Results/ARMN_dat/tAR_f_0.6_ps_0.3_km_10_kn_6.dat",sep="",header = F)
-x<-read.delim("./ARMN_Results/ARMN_dat/tMN_f_0.6_ps_0.3_km_10_kn_6.dat",sep="",header = F)
-colnames(x)<-c("t","M","N")
-Meq<-x$M
-Meq<-tail(Meq,1)
-Neq<-x$N
-Neq<-tail(Neq,1)
-Plotter_AR(f=f,KM=10,KN=6,Meq,Neq,eM=0.5,eN=0.5,aM=0.1,aN=0.2,bmax=0.8,d=0.5,s=0.1,
-           x1,xlm,ylm,n=10000,resloc=resloc,nametag="phi_5_",plot_MZNGI = T, plot_NZNGI = T)
+#f<-0.6
+#xlm<-c(0,450)
+#ylm<-c(0,50)
+#x1<-read.delim("./ARMN_Results/ARMN_dat/tAR_f_0.6_ps_0.3_km_10_kn_6.dat",sep="",header = F)
+#x<-read.delim("./ARMN_Results/ARMN_dat/tMN_f_0.6_ps_0.3_km_10_kn_6.dat",sep="",header = F)
+#colnames(x)<-c("t","M","N")
+#Meq<-x$M
+#Meq<-tail(Meq,1)
+#Neq<-x$N
+#Neq<-tail(Neq,1)
+#Plotter_AR(f=f,KM=10,KN=6,Meq,Neq,eM=0.5,eN=0.5,aM=0.1,aN=0.2,bmax=0.8,d=0.5,s=0.1,
+#           x1,xlm,ylm,n=10000,resloc=resloc,nametag="phi_5_",plot_MZNGI = T, plot_NZNGI = T)
 
 #======================================================================================================
 
@@ -194,6 +200,7 @@ Plotter_ARMN_vs_t<-function(x1,xlm,ylm,nametag,taglegend,resloc){
   plot(x1[,1],x1[,2],xlab="time",ylab="",cex.lab=2.5,cex.axis=2,
        col="black",type="l",xlim=xlm,
        ylim=ylm,lwd=2)
+  abline(h=0,col="grey")
   #title(main=bquote(C[c]^0 == .(phi0)),cex.main=2.5,line=-8,adj=0.8)
   lines(x1[,1],x1[,3],col="black",lty="dashed",lwd=2)
   legend("topright", taglegend, col = c("black", "black"),
@@ -213,9 +220,9 @@ resloc<-"./ARMN_Results/"
 xC<-read.delim("./ARMN_Results/ARMN_dat/tAR_f_0.2_ps_0.3_km_10_kn_10.dat",sep="",header = F)
 xS<-read.delim("./ARMN_Results/ARMN_dat/tMN_f_0.2_ps_0.3_km_10_kn_10.dat",sep="",header = F)
 
-Plotter_ARMN_vs_t(x1=xC,xlm=c(0,1000),ylm=c(0,1000),nametag="phi_5_f_0.2_KM_10_KN_10_AR",
+Plotter_ARMN_vs_t(x1=xC,xlm=c(0,1000),ylm=c(0,100),nametag="phi_5_f_0.2_KM_10_KN_10_AR",
                   taglegend=c("A","R"), resloc)
-Plotter_ARMN_vs_t(x1=xS,xlm=c(0,1000),ylm=c(0,50),nametag="phi_5_f_0.2_KM_10_KN_10_MN",
+Plotter_ARMN_vs_t(x1=xS,xlm=c(0,1000),ylm=c(0,10),nametag="phi_5_f_0.2_KM_10_KN_10_MN",
                   taglegend=c("M","N"),resloc)
 
 #----------for fmin < f < fmax ---------
@@ -241,13 +248,13 @@ Plotter_ARMN_vs_t(x1=xS,xlm=c(0,1000),ylm=c(0,1),nametag="phi_5_f_0.6_KM_10_KN_1
 # ------------------ call the function for --------------- KM not equal to KN ---------------
 
 # ----- for fmin < f < fmax ------
-xC<-read.delim("./ARMN_Results/ARMN_dat/tAR_f_0.6_ps_0.3_km_10_kn_6.dat",sep="",header = F)
-xS<-read.delim("./ARMN_Results/ARMN_dat/tMN_f_0.6_ps_0.3_km_10_kn_6.dat",sep="",header = F)
+#xC<-read.delim("./ARMN_Results/ARMN_dat/tAR_f_0.6_ps_0.3_km_10_kn_6.dat",sep="",header = F)
+#xS<-read.delim("./ARMN_Results/ARMN_dat/tMN_f_0.6_ps_0.3_km_10_kn_6.dat",sep="",header = F)
 
-Plotter_ARMN_vs_t(x1=xC,xlm=c(0,1000),ylm=c(0,60),nametag="phi_5_f_0.6_KM_10_KN_6_AR",
-                  taglegend=c("A","R"), resloc)
-Plotter_ARMN_vs_t(x1=xS,xlm=c(0,1000),ylm=c(0,3),nametag="phi_5_f_0.6_KM_10_KN_6_MN",
-                  taglegend=c("M","N"),resloc)
+#Plotter_ARMN_vs_t(x1=xC,xlm=c(0,1000),ylm=c(0,60),nametag="phi_5_f_0.6_KM_10_KN_6_AR",
+#                  taglegend=c("A","R"), resloc)
+#Plotter_ARMN_vs_t(x1=xS,xlm=c(0,1000),ylm=c(0,3),nametag="phi_5_f_0.6_KM_10_KN_6_MN",
+#                  taglegend=c("M","N"),resloc)
 
 #==============================================================================================
 # plotter fn to get plots of (A,R) or (M,N) vs. ps and f
@@ -338,28 +345,63 @@ plot_ARMN_vs_ps_f(x1,resloc,nametag,xlb,taglegend,axlim)
 
 
 # ==================================================================================================
+source("./get_MNAR_eqm.R")
   multi_plotter<-function(resloc,figname){
     if(figname=="M_by_N_eqm_vs_phi"){
-     
+      
+      #------------------ analytical expression results -----------------------------------------------
+      x<-c(0.5,0.5,0.5,0.5)
+      KM<-10
+      KN<-10
+      f<-0.3
+      ps<-0.3
+      aM<-0.1
+      aN<-0.2
+      KA<-5
+      d<-0.5
+      bmax<-0.8
+      s<-0.1
+      #phi=5
+      u<-0.4
+      eM<-0.5
+      eN<-0.5
+      
+      phi_seq<-seq(from=1,to=10,by=0.1)
+      
+      x1<-data.frame(phi=phi_seq,M_by_N_eqm=NA)
+      
+      for(i in c(1:length(phi_seq))){
+        phi<-phi_seq[i]
+        #cat(phi,"\n")
+        ans<-get_MNAR_eqm_analytical(x=x,KM=KM,KN=KN,f=f,ps=ps,aM=aM,aN=aN,KA=KA,d=d,bmax=bmax,s=s,phi=phi,u=u,eM=eM,eN=eN)
+        Meq<-ans$Meq
+        Neq<-ans$Neq
+        x1$M_by_N_eqm[i]<-Meq/Neq
+      }
+      
       pdf(paste(resloc,"M_by_N_eqm_vs_phi.pdf",sep=""),width=8,height=8)
-     
-      x1<-read.delim("./ARMN_Results/ARMN_dat/ARMN_phi_vary_km_10_kn_10_f_0.3_ps_0.3.dat",sep="",header = F)
       op<-par(mar=c(6,6,2,2),mgp=c(3,1,0),pty="s")
       plot(x1[,1],x1[,2],xlab="D",ylab=c(expression(hat(M)/hat(N))),cex.lab=2.5,cex.axis=2,col="black",type="l",xlim=c(1,10),
            ylim=c(0,7),lwd=2)
       abline(h=1,col="black",lty="dotted",lwd=2)
-      #legend("topright", c(expression(hat(M)/hat(N))), 
-      #       cex = 2.3, lty = c(1), lwd=c(2), xpd = TRUE, horiz = F, inset = c(0,0),y.intersp = 1,x.intersp = 0.2,
-      #       bty = "n")  
       par(op)
       dev.off()
+     #------------- numeric ODE solution: from fortran output --------------------------------------
+      #pdf(paste(resloc,"M_by_N_eqm_vs_phi.pdf",sep=""),width=8,height=8)
+      #x1<-read.delim("./ARMN_Results/ARMN_dat/ARMN_phi_vary_km_10_kn_10_f_0.3_ps_0.3.dat",sep="",header = F)
+      #op<-par(mar=c(6,6,2,2),mgp=c(3,1,0),pty="s")
+      #plot(x1[,1],x1[,2],xlab="D",ylab=c(expression(hat(M)/hat(N))),cex.lab=2.5,cex.axis=2,col="black",type="l",xlim=c(1,10),
+      #     ylim=c(0,7),lwd=2)
+      #abline(h=1,col="black",lty="dotted",lwd=2)
+      #par(op)
+      #dev.off()
     }else if(figname=="Puptake_vs_M_N"){
       # Plotting P uptake function by AMF
       # 3D figure
       
       pdf(paste(resloc,"Puptake_vs_M_N.pdf",sep=""),width=8,height=8)
      
-      op<-par(mar=c(6,6,2,2),pty="s")
+      op<-par(mar=c(2,2,2,2),mgp=c(3,0.5,0),pty="s")
       f<-0.3
       u<-0.4
       KA<-5
@@ -368,10 +410,10 @@ plot_ARMN_vs_ps_f(x1,resloc,nametag,xlb,taglegend,axlim)
       PUfun <- function(M,N){(M/(M+KA))*u*((M/(M+N))/(1-f+(f*(M/(M+N)))))}
       PU    <- outer(M,N, FUN="PUfun")
       
-      persp(M,N,PU,theta = -45, phi = 25,col = "grey",xlab="Mutualist (M)",
+      persp(M,N,PU,theta = -50, phi = 25,col = "grey",xlab="Mutualist (M)",
             ylab="Non-mutualist (N)",
             zlab="P-uptake via AMF (F)",ticktype = "detailed",
-            cex.lab=1.5,cex.axis=1.5)
+            cex.lab=1.5,cex.axis=1.4)
       par(op)
       dev.off()
     }else if(figname=="schematic_diagram"){
@@ -381,20 +423,22 @@ plot_ARMN_vs_ps_f(x1,resloc,nametag,xlb,taglegend,axlim)
       linepos<- -34
       linepos2<- -11.5
      
-      op<-par(mar=c(6,6,2,2),pty="s")
-      plot(-1,-2,xlim=c(0,0.8),ylim=c(0,0.8),xlab="R", ylab="A",
-           xaxt="n",yaxt="n",cex.lab=1.8)
+      op<-par(mar=c(3,3,2,2),mgp=c(1,1,0),pty="s")
+      plot(-1,-2,xlim=c(0,0.8),ylim=c(0,1),xlab="x", ylab="y",
+           xaxt="n",yaxt="n",cex.lab=3)
       abline(a=0.5,b=-0.7,lwd=2)
       abline(a=0.7,b=-2.5,lty="dashed",lwd=2)
-      legend("topright", c("Mutualist","Non-mutualist"), 
-             cex = 1.5, lty = c(1, 2), lwd=c(2,2), xpd = TRUE, horiz = F, inset = c(0,0),y.intersp = 1,x.intersp = 0.2,
+      #text1<-bquote("-ea"[M])
+      legend("top", c(expression("Equation (8), from the mutualist model equation. Slope -ea"[M]*"."),
+                      expression("Equation (9), from the non-mutualist model equation. Slope -ea"[N]*"/(1-f).")), 
+             cex = 1.2, lty = c(1, 2), lwd=c(2,2), xpd = TRUE, horiz = F, inset = c(0,0),y.intersp = 2,x.intersp = 0.2,
              bty = "n") 
      # mtext(adj=0.65,line=-15,(bquote("A"[M]^"*"~"= C"[cM]^"*")),cex=1.5)
     #  mtext(adj=0.7,line=-18,(bquote("A"[N]^"*"~"= C"[cN]^"*"~"/(1-f)")),cex=1.5)
-      mtext(adj=0.96,line=linepos,(bquote("R"[M]^"*")),cex=1.5)
-      mtext(adj=0.4,line=linepos,(bquote("R"[N]^"*")),cex=1.5)
-      mtext(adj=-0.08,line=linepos2,(bquote("A"[M]^"*")),cex=1.5)
-      mtext(adj=-0.08,line=-2.6,(bquote("A"[N]^"*")),cex=1.5)
+    #  mtext(adj=0.96,line=linepos,(bquote("R"[M]^"*")),cex=1.5)
+    #  mtext(adj=0.4,line=linepos,(bquote("R"[N]^"*")),cex=1.5)
+    #  mtext(adj=-0.08,line=linepos2,(bquote("A"[M]^"*")),cex=1.5)
+    #  mtext(adj=-0.08,line=-2.6,(bquote("A"[N]^"*")),cex=1.5)
       par(op)
       dev.off()
     }
