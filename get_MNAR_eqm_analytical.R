439269e3739e00c34809926e1d48d76ff34bcd76
sghosh89 <ghoshshyamolina89@gmail.com>
2020-04-15 20:13:26 -0500

    edit

--- get_MNAR_eqm_analytical.R
+++ get_MNAR_eqm_analytical.R
@@ -4,27 +4,48 @@
 # KM=KN
 # eM=eN
 
-get_MNAR_eqm_analytical<-function(x,KM=10,KN=10,f,ps,aM=0.1,aN=0.2,KA=5,d=0.5,bmax=0.8,s=0.1,phi=5,u=0.4,eM=0.5,eN=0.5){
+get_MNAR_eqm_analytical<-function(f,ps=0.3,s=0.1,aM=0.1,aN=0.2,phi=5,getalleqmval=F){
   
-  if(KM!=KN | eM!=eN){
-    stop("Error: KM, KN are not equal and/ or eM, eN are not equal",call. = T)
+  # defining parameters
+  #phi<-5
+  #aM<-0.1 
+  #aN<-0.2
+  d<-0.5
+  bmax<-0.8
+  #s<-0.1
+  e<-0.5
+  u<-0.4
+  K<-10
+  KA<-5
+  
+  # get Req
+  Req1<-((1-f)*K*d)/(e*(aN-aM*(1-f)))
+  Req2<-(1/((1-f)*(bmax-d))) - (1/((bmax*(1-s))-d))
+  Req<-Req1*Req2
+  
+  #get Meq
+  yeq<- (K*d)/((bmax*(1-s))-d)-(e*aM*Req) # this is A by alpha at eqm from mutualist ZNGI eqn.
+  tempo<-(1-ps)/(u*yeq)
+  
+  Meq<-0.5*(tempo+sqrt((tempo^2)+(4*tempo*KA)))
+  
+  # get Neq
+  Neq<-((phi/Req) -(aM*Meq))/aN
+  
+  #get Aeq
+  PMeq<-Meq/(Meq+Neq)
+  alphaeq<-(Meq+Neq)*(1-f+(f*PMeq))
+  Aeq<-yeq*alphaeq
+  
+  if(getalleqmval==T){
+    return(list(Aeq=Aeq,
+                Req=Req,
+                Meq=Meq,
+                Neq=Neq))
   }
   
-  fmin<- (s*bmax)/(bmax-d)
-  K<-KM<-KN
-  e<-eM<-eN
+  if(getalleqmval==F){
+    return(Neq) # to get uniroot solution
+  }
   
-  temp<-(aN-(aM*(1-f)))*(bmax-d)
-    
-  Req<- (K*d*(f-fmin))/(e*(1-fmin)*temp)
-  A_by_alpha_eqm<- ((K*d*(bmax-d)*(aN-aM))+(s*K*d*bmax*aM))/((bmax*(1-s)-d)*temp)
-  Meq<-((1-ps)+sqrt(((1-ps)^2)+(4*u*KA*A_by_alpha_eqm*(1-ps))))/(2*u*A_by_alpha_eqm)
-  Neq<-((phi/Req)-(aM*Meq))/aN
-  Aeq<-A_by_alpha_eqm*(Meq+((1-f)*Neq))
-  
-  return(list(Meq=Meq,
-           Neq=Neq,
-           Aeq=Aeq,
-           Req=Req))
 }
-
--- get_fmax.R
+++ get_fmax.R
@@ -4,48 +4,7 @@
 library(rootSolve)
 #library(nleqslv)
 
-myfun<-function(f,s=0.1,aM=0.1,aN=0.2,phi=5){
-  
-  # defining parameters
-  #phi<-5
-  #aM<-0.1 
-  #aN<-0.2
-  d<-0.5
-  bmax<-0.8
-  #s<-0.1
-  e<-0.5
-  u<-0.4
-  ps<-0.3
-  K<-10
-  KA<-5
-  
-  # get Req
-  Req1<-((1-f)*K*d)/(e*(aN-aM*(1-f)))
-  Req2<-(1/((1-f)*(bmax-d))) - (1/((bmax*(1-s))-d))
-  Req<-Req1*Req2
-  
-  #get Meq
-  yeq<- (K*d)/((bmax*(1-s))-d)-(e*aM*Req)
-  tempo<-(1-ps)/(u*yeq)
-  
-  Meq<-0.5*(tempo+sqrt((tempo^2)+(4*tempo*KA)))
-  
-  # get Neq
-  Neq<-((phi/Req) -(aM*Meq))/aN
-  
-  #get Aeq
-  PMeq<-Meq/(Meq+Neq)
-  alphaeq<-(Meq+Neq)*(1-f+(f*PMeq))
-  Aeq<-yeq*alphaeq
-  
-  #return(list(Aeq=Aeq,
-  #            Req=Req,
-  #            Meq=Meq,
-  #            Neq=Neq))
-  
-  return(Neq) # to get uniroot solution
-}
-
+source("get_MNAR_eqm_analytical.R")
 #===============================
 # Now, call the function
 d<-0.5
@@ -54,17 +13,17 @@ bmax<-0.8
 s<-0.1
 fmin<-(s*bmax)/(bmax-d)
 #fmin
-#curve(myfun, xlim=c(fmin,1), lwd=2, lty=2, ylab='Neq',xlab='f',ylim=c(-0.1,1))
+#curve(get_MNAR_eqm_analytical, xlim=c(fmin,1), lwd=2, lty=2, ylab='Neq',xlab='f',ylim=c(-0.1,1))
 #abline(h=0)
-myfmax<-uniroot(myfun, interval=c(fmin,0.9)) 
-fmax<-myfmax$root
+myfmax<-uniroot(get_MNAR_eqm_analytical, interval=c(fmin,0.9),ps=0.3,s=0.1,aM=0.1,aN=0.2,phi=5,getalleqmval=F) 
+fmax<-myfmax$root #
 cat("range of fidelity for coexistence: (fmin,fmax)=(",fmin,",",fmax,")\n")
 (fmax-fmin)
 
 #------------------ when aN=aM=0.1, s=0.1
 s<-0.1
 fmin<-(s*bmax)/(bmax-d)
-myfmax<-uniroot(myfun, interval=c(fmin,0.9), s=s, aM=0.1, aN=0.1, phi=5) #fmax=0.3146655
+myfmax<-uniroot(get_MNAR_eqm_analytical, interval=c(fmin,0.9), ps=0.3,s=0.1,aM=0.1,aN=0.1,phi=5,getalleqmval=F) #fmax=0.3146655
 fmax<-myfmax$root
 cat("range of fidelity for coexistence: (fmin,fmax)=(",fmin,",",fmax,")\n")
 (fmax-fmin)
@@ -72,7 +31,7 @@ cat("range of fidelity for coexistence: (fmin,fmax)=(",fmin,",",fmax,")\n")
 #----------------- when s=0, aN>aM 
 s<-0
 fmin<-(s*bmax)/(bmax-d)
-myfmax<-uniroot(myfun, interval=c(fmin,0.9), s=s, aM=0.1, aN=0.2, phi=5) #fmax=0.2103166
+myfmax<-uniroot(get_MNAR_eqm_analytical, interval=c(fmin,0.9), ps=0.3, s=s, aM=0.1, aN=0.2, phi=5, getalleqmval=F) #fmax=0.2103166
 fmax<-myfmax$root
 cat("range of fidelity for coexistence: (fmin,fmax)=(",fmin,",",fmax,")\n")
 (fmax-fmin)
@@ -98,11 +57,11 @@ while(s<=smax){
   
   #---------- aM=0.1, aN=0.2 ---------------
   
-  myfmax<-uniroot(myfun, interval=c(fmin+0.0001,0.999), s=s, aM=0.1, aN=0.2, phi=5) 
+  myfmax<-uniroot(get_MNAR_eqm_analytical, interval=c(fmin+0.0001,0.999), ps=0.3, s=s, aM=0.1, aN=0.2, phi=5, getalleqmval=F) 
   fmax_phi5<-myfmax$root
   fmaxs_phi5_aM_0.1_aN_0.2<-c(fmaxs_phi5_aM_0.1_aN_0.2,fmax_phi5)
   
-  myfmax<-uniroot(myfun, interval=c(fmin+0.0001,0.999), s=s, aM=0.1, aN=0.2, phi=1) 
+  myfmax<-uniroot(get_MNAR_eqm_analytical, interval=c(fmin+0.0001,0.999), ps=0.3, s=s, aM=0.1, aN=0.2, phi=1, getalleqmval=F) 
   fmax_phi1<-myfmax$root
   fmaxs_phi1_aM_0.1_aN_0.2<-c(fmaxs_phi1_aM_0.1_aN_0.2,fmax_phi1)
   
@@ -153,11 +112,11 @@ while(s<=smax){
     fmin<-(s*bmax)/(bmax-d) # indep. of phi, aM or aN
     fmins<-c(fmins,fmin)
     
-    myfmax<-uniroot(myfun, interval=c(fmin+0.001,0.9), s=s, aM=0.1, aN=0.1, phi=5) 
+    myfmax<-uniroot(get_MNAR_eqm_analytical, interval=c(fmin+0.001,0.9), ps=0.3, s=s, aM=0.1, aN=0.1, phi=5, getalleqmval=F) 
     fmax_phi5<-myfmax$root
     fmaxs_phi5_aM_0.1_aN_0.1<-c(fmaxs_phi5_aM_0.1_aN_0.1,fmax_phi5)
     
-    myfmax<-uniroot(myfun, interval=c(fmin+0.001,0.9), s=s, aM=0.1, aN=0.1, phi=1) 
+    myfmax<-uniroot(get_MNAR_eqm_analytical, interval=c(fmin+0.001,0.9), ps=0.3, s=s, aM=0.1, aN=0.1, phi=1, getalleqmval=F) 
     fmax_phi1<-myfmax$root
     fmaxs_phi1_aM_0.1_aN_0.1<-c(fmaxs_phi1_aM_0.1_aN_0.1,fmax_phi1)
 #  }
@@ -192,22 +151,29 @@ dev.off()
 
 
 #======================================================================================================
-# To see plot comment the uniroot return line in the myfun and uncomment the other return statement
-ans<-myfun(f=fmin)
-(Aeq<-ans$Aeq)
-(Req<-ans$Req)
-(Meq<-ans$Meq)
-(Neq<-ans$Neq)
-
 # Analytical plot of eqm soln vs fidelity
 Aeqs<-c()
 Reqs<-c()
 Meqs<-c()
 Neqs<-c()
-frange<-seq(from=fmin,to=1,by=0.01)
+
+s<-0.1
+bmax<-0.8
+d<-0.5
+fmin<-(s*bmax)/(bmax-d)
+ps<-0.3
+aM<-0.1
+aN<-0.2
+phi<-5
+myfmax<-uniroot(get_MNAR_eqm_analytical, interval=c(fmin,0.9), ps=ps,s=s,aM=aM,aN=aN,phi=phi,getalleqmval=F) #fmax=0.3146655
+fmax<-myfmax$root
+
+
+frange<-seq(from=fmin,to=fmax,by=0.01)
+
 for(f in frange){
   #cat("f=",f,"\n")
-  ans<-myfun(f=f)
+  ans<-get_MNAR_eqm_analytical(f=f,ps=ps,s=s,aM=aM,aN=aN,phi=phi,getalleqmval=T)
   Aeqs<-c(Aeqs,ans$Aeq)
   Reqs<-c(Reqs,ans$Req)
   Meqs<-c(Meqs,ans$Meq)
@@ -217,10 +183,10 @@ for(f in frange){
 pdf("./ARMN_Results/analytical_MNeqm_vs_f_ps_0.3_KM_10_KN_10_phi_5.pdf",width=8,height=8)
 op<-par(mar=c(6,6,2,2),pty="s")
 ylm<-round(max(Meqs,Neqs[-1]))
-plot(frange,Meqs,type="l",ylab="",xlab="f",ylim=c(-1,ylm),xlim=c(fmin,1),lwd=2,cex.lab=2.5,cex.axis=2)
+plot(frange,Meqs,type="l",ylab="",xlab="f",ylim=c(-1,ylm),xlim=c(fmin,fmax),lwd=2,cex.lab=2.5,cex.axis=2)
 lines(frange,Neqs,lty="dashed",lwd=2)
-abline(h=0,col="red")
-points(x=0.4947031,y=0,cex=1.5,pch=19) # this is the fmax
+abline(h=0,col="gray")
+#points(x=0.4947031,y=0,cex=1.5,pch=19) # this is the fmax
 
 legend("topright", c(expression(hat(M)),expression(hat(N))), col = c("black", "black"),
        cex = 2.5, lty = c(1, 2), lwd=c(2,2), xpd = TRUE, horiz = F, inset = c(0,0),y.intersp = 1.2,x.intersp = 0.2,
@@ -232,10 +198,10 @@ dev.off()
 pdf("./ARMN_Results/analytical_AReqm_vs_f_ps_0.3_KM_10_KN_10_phi_5.pdf",width=8,height=8)
 op<-par(mar=c(6,6,2,2),pty="s")
 ylm<-round(max(Reqs,Aeqs[-1]))
-plot(frange,Aeqs,type="l",ylab="",xlab="f",ylim=c(-1,ylm),xlim=c(fmin,1),lwd=2,cex.lab=2.5,cex.axis=2)
+plot(frange,Aeqs,type="l",ylab="",xlab="f",ylim=c(-1,ylm),xlim=c(fmin,fmax),lwd=2,cex.lab=2.5,cex.axis=2)
 lines(frange,Reqs,lty="dashed",lwd=2)# Beyond fmax~0.49, Reqm should be constant as D/aM*Meqm 
                                     # as Neqm goes to 0 then the expression for Reqm is not valid there.
-abline(h=0,col="red")
+abline(h=0,col="gray")
 
 legend("topright", c(expression(hat(A)),expression(hat(R))), col = c("black", "black"),
        cex = 2.5, lty = c(1, 2), lwd=c(2,2), xpd = TRUE, horiz = F, inset = c(0,0),y.intersp = 1.2,x.intersp = 0.2,
