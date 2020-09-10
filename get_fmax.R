3b7892a714a03edf55da598b3ee31d5b6ed8e5bd
sghosh89 <ghoshshyamolina89@gmail.com>
2020-09-10 14:35:31 -0500

    fig 5 contour plot suggested by am nat assoc ed

--- get_fmax.R
+++ get_fmax.R
@@ -150,6 +150,136 @@ dev.off()
 
 #======================================================================================================
 
+#------------------ Contour plots suggested by Am. Nat.'s Erol Akcay -----------------------
+
+# contour plot of Meq/(Meq+Neq) vs. fidelity,f vs. constant new root growth rate, phi
+
+get_phi_f_PM_data<-function(aM,aN,s){
+  
+  # start of a function here
+  ps<-0.3
+  bmax<-0.8
+  d<-0.5
+  fmin<-(s*bmax)/(bmax-d)
+  
+  phi_range<-seq(from=1,to=5,by=1/50)
+  
+  phi_and_fmax<-data.frame(phi=phi_range,fmax=NA)
+  
+  for(i in 1:length(phi_range)){
+    phi<-phi_range[i]
+    cat("phi=",phi,"\n")
+    
+    myfmax<-uniroot(get_MNAR_eqm_analytical, interval=c(fmin,200), # ideally the interval we should look (fmin,1) 
+                    ps=ps,s=s,aM=aM,aN=aN,phi=phi,getalleqmval=F) 
+    fmax<-myfmax$root
+    
+    # if(fmax>1){
+    #  fmax<-1 # force set fmax = 1 
+    #stop("Stop code: fmax is not less that 1 for a given ps",call.=T)
+    #}
+    
+    
+    phi_and_fmax$fmax[i]<-fmax
+  }
+  
+  
+  # Now make a table for all possible combo of phi, f for co-existence 
+  # We varied f in between >= fmin to < fmax for a given phi
+  
+  len<-15000 # give it a big number
+  
+  phi_f_PM<-data.frame(phi=NA*numeric(len),
+                       f=NA*numeric(len),
+                       PM=NA*numeric(len),
+                       Aeq=NA*numeric(len),
+                       Req=NA*numeric(len),
+                       Meq=NA*numeric(len),
+                       Neq=NA*numeric(len))
+  
+  k<-1
+  finit<-fmin#0
+  for(i in c(1:nrow(phi_and_fmax))){
+    phi<-phi_and_fmax$phi[i]
+    #fmax<-1
+    fmax<-phi_and_fmax$fmax[i] 
+    
+    #cat("ps=",ps,"fmax=",fmax,"\n")
+    
+    f_range<-seq(from=finit,to=fmax,by=(fmax-finit)/50)
+    
+    for(j in c(1:length(f_range))){
+      
+      f<-f_range[j]
+      
+      ans<-get_MNAR_eqm_analytical(f=f,ps=ps,s=s,aM=aM,aN=aN,phi=phi,getalleqmval=T)
+      PM<-ans$Meq/(ans$Meq+ans$Neq)
+      
+      phi_f_PM$f[k]<-f
+      phi_f_PM$phi[k]<-phi
+      phi_f_PM$PM[k]<-PM
+      phi_f_PM$Aeq[k]<-ans$Aeq
+      phi_f_PM$Req[k]<-ans$Req
+      phi_f_PM$Meq[k]<-ans$Meq
+      phi_f_PM$Neq[k]<-ans$Neq
+      cat("k=",k,"f=",f,"phi=",phi,"PM=",PM,"\n")
+      k<-k+1
+    }
+  }
+  
+  (z<-phi_f_PM[which(phi_f_PM$PM>1),]) # this table shows at f=fmax, PM should be 1 but instead it's slightly >1
+  # I think, this is because uniroot function just finds the root (that could be 0,>0,<0) and these are numerical precession error
+  # but for meaningful biological variable Neq can't be negative, so we can consider 
+  # Neq goes to zero and PM = 1 for given phi, f combination
+  
+  ind<-which(phi_f_PM$PM>1)
+  phi_f_PM[ind,]$PM<-1
+  
+  phi_f_PM<-na.omit(phi_f_PM) # just to omit unnecessary blank lines
+  
+  return(phi_f_PM)
+}
+
+# call the function now
+
+# for aM = aN, s=0.1
+aM<-0.1
+aN<-0.1
+s<-0.1
+ans<-get_phi_f_PM_data(aM=aM,aN=aN,s=s)
+write.csv(ans,paste("./ARMN_Results/phi_f_PM_aM_",aM,"_aN_",aN,"_s_",s,".csv",sep=""),row.names = F)
+
+# for aM = aN, s=0.2
+aM<-0.1
+aN<-0.1
+s<-0.2
+ans<-get_phi_f_PM_data(aM=aM,aN=aN,s=s)
+write.csv(ans,paste("./ARMN_Results/phi_f_PM_aM_",aM,"_aN_",aN,"_s_",s,".csv",sep=""),row.names = F)
+
+# for aM < aN, s=0.1
+aM<-0.1
+aN<-0.2
+s<-0.1
+ans<-get_phi_f_PM_data(aM=aM,aN=aN,s=s)
+write.csv(ans,paste("./ARMN_Results/phi_f_PM_aM_",aM,"_aN_",aN,"_s_",s,".csv",sep=""),row.names = F)
+
+# for aM < aN, s=0.2
+aM<-0.1
+aN<-0.2
+s<-0.2
+ans<-get_phi_f_PM_data(aM=aM,aN=aN,s=s)
+write.csv(ans,paste("./ARMN_Results/phi_f_PM_aM_",aM,"_aN_",aN,"_s_",s,".csv",sep=""),row.names = F)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
